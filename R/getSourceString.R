ee673685f8a1c0498eac8e941d27a9f270870e44
Jordan S Read <jread@usgs.gov>
2013-06-27 06:37:04 -0500

    creating private functions for duplicated code

---
+++ R/getSourceString.R
@@ -0,0 +1,16 @@
+getSourceString = function(sourceContent){
+	
+	subTypeDataSrc <- names(sourceSynonyms)
+	useI	<-	vector(length = length(subTypeDataSrc))
+
+	for (i in 1:length(sourceSynonyms)){
+	  for (k in 1:length(sourceSynonyms[[i]])){
+	    if (grepl(as.character(sourceSynonyms[[i]][k]),sourceContent)){
+	      useI[i] = TRUE
+	    }
+	  }
+	}
+	sourceString	<-	paste(c('Data sources:',
+		paste(c(subTypeDataSrc[useI]),collapse=', ')),collapse=' ')
+	return(sourceString)
+}
