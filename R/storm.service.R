1aefe61b402d85b35cf073f2c8b3d671c74f86e5
Jordan S Read <jread@usgs.gov>
2013-06-27 06:37:28 -0500

    set to use private functions, set shared naming

--- R/storm.service.R
+++ R/storm.service.R
@@ -2,38 +2,28 @@
 storm.service = function(serviceEndpoint,attribute){
 	subType	<-	attribute
 
-	subTypeDataSrc <- names(synDataSrc)
+	subTypeDataSrc <- names(sourceSynonyms)
 
 	doc <- xmlInternalTreeParse(serviceEndpoint)
 
 	title <- xmlValue(getNodeSet(doc,'//citation/citeinfo/title')[[1]])
 
 	dataSrc <-  sapply(getNodeSet(doc,'//dataqual/lineage/srcinfo/srccite/citeinfo/title'),xmlValue)
-	sourceStr <-  paste(dataSrc,collapse='|')
-
-	useI = vector(length = length(subTypeDataSrc))
-
-	for (i in 1:length(synDataSrc)){
-	  for (k in 1:length(synDataSrc[[i]])){
-	    if (grepl(as.character(synDataSrc[[i]][k]),sourceStr)){
-	      useI[i] = TRUE
-	    }
-	  }
-	}
-
+	sourceContent <-  paste(dataSrc,collapse='|')
+	
 	baseType<- substring(subType,1,4)
 	stormNum <- substring(subType,5)
 
-	detail <- paste(c('These probabilities apply to a generic representation of a category',
+	overview <-  paste(c('This datasets includes an element of ', title),collapse='')
+	attrDefinition  <- attrDefinitions[baseType]
+	processDetail <- paste(c('These probabilities apply to a generic representation of a category',
 	                  stormNum,'hurricane'),collapse=' ')
-	definition  <- typeDef[baseType]
-	firstLine <-  paste(c('This datasets includes an element of ', title),collapse='')
-	dataSources <- paste(c('Data sources:',paste(c(subTypeDataSrc[useI]),collapse=', ')),collapse=' ')
-	summary <- paste(c(firstLine,definition,detail,dataSources),collapse='. ')
+	sourceString	<-	getSourceString(sourceContent)
 	
+	medium.summary <- paste(c(overview,attrDefinition,processDetail,sourceString),collapse='. ')
 	summaryJSON	<- toJSON(list('summary'=list(
 		'tiny'=list('text'='xxx'),
-		'medium'=list('title'='XXX','text'=summary),
+		'medium'=list('title'='XXX','text'=medium.summary),
 		'full'=list('title'='XXX','text'='XXX','publications'='XXX'))), method="C" )
 	return(summaryJSON)
 }
