5a9bd8ce0dbdd421d01f07f4003a281eb8e6929c
Jordan S Read <jread@usgs.gov>
2013-07-11 07:14:02 -0500

    added is.generic test for storm service

--- R/storm.service.R
+++ R/storm.service.R
@@ -11,25 +11,32 @@ storm.service = function(serviceEndpoint,attribute){
 	dataSrc <-  sapply(getNodeSet(doc,'//dataqual/lineage/srcinfo/srccite/citeinfo/title'),xmlValue)
 	sourceContent <-  paste(dataSrc,collapse='|')
 	
-	baseType<- substring(subType,1,4)
-	stormNum <- substring(subType,5)
+	is.generic	<-	FALSE
+	if (!is.na(as.numeric(substr(subType,nchar(subType),nchar(subType))))){
+		is.generic	<-	TRUE
+		stormNum	<- substring(subType,5)
+		subType	<-		substring(subType,1,4)
+		processDetail <- paste(c('These probabilities apply to a generic representation of a category',
+		                  stormNum,'hurricane'),collapse=' ')
+	}
+
+	
 
 	overview <-  paste(c('This datasets includes an element of ', title),collapse='')
-	attrDefinition  <- attrDefinitions[baseType]
-	processDetail <- paste(c('These probabilities apply to a generic representation of a category',
-	                  stormNum,'hurricane'),collapse=' ')
+	attrDefinition  <- attrDefinitions[subType]
+	
 	sourceString	<-	getSourceString(sourceContent)
 	
 	medium.summary	<-	paste(c(overview,attrDefinition,processDetail,sourceString),collapse='. ')
 	
 	# create medium title for storm item
 	location	<-	getLocationString(overview) # default call is to medium service
-	medium.title	<-	paste(c(titleMap[['medium']][[baseType]],'during a category',stormNum,'storm in',location),collapse=' ')
+	medium.title	<-	paste(c(titleMap[['medium']][[subType]],'during a category',stormNum,'storm in',location),collapse=' ')
 	
 	location	<-	getLocationString(overview,size='tiny')
-	tiny.text	<-	paste(c(titleMap[['tiny']][[baseType]],', CAT',stormNum,' storm in ',location),collapse='')
+	tiny.text	<-	paste(c(titleMap[['tiny']][[subType]],', CAT',stormNum,' storm in ',location),collapse='')
 	
-	full.title	<-	paste(c('The Category ',stormNum,' ',titleMap[['full']][[baseType]],' element of ',title),collapse='')
+	full.title	<-	paste(c('The Category ',stormNum,' ',titleMap[['full']][[subType]],' element of ',title),collapse='')
 	full.text	<-	sub('\n','',paste(c(abstract),collapse=''))
 	
 	onlinks	<-	getNodeSet(doc,'//citeinfo/onlink')
