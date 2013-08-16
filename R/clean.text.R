7a7c852401b969993b0a00e4363b41649557e325
Jordan S Read <jread@usgs.gov>
2013-08-16 07:46:01 -0500

    fixed=TRUE

--- R/clean.text.R
+++ R/clean.text.R
@@ -1,8 +1,8 @@
 clean.text	=	function(text){
 	
-	text	<-	sub('\\n\\n','',text)
-	text	<-	sub('\n','',text)
-	text	<-	sub('.. ','. ',text)
-	text	<-	sub('. . ','. ',text)
+	text	<-	sub('\\n\\n','',text,fixed = TRUE)
+	text	<-	sub('\n','',text,fixed = TRUE)
+	text	<-	sub('..','.',text,fixed = TRUE)
+	text	<-	sub('. .','. ',text,fixed = TRUE)
 	return(text)
 }
