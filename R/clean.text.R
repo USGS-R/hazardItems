4b66ce67c4e508f81791a401e012fe2851e0d798
Jordan S Read <jread@usgs.gov>
2013-08-16 08:14:31 -0500

    new replaces

--- R/clean.text.R
+++ R/clean.text.R
@@ -1,8 +1,10 @@
 clean.text	=	function(text){
 	
-	text	<-	sub('\\n\\n','',text,fixed = TRUE)
-	text	<-	sub('\n','',text,fixed = TRUE)
-	text	<-	sub('..','.',text,fixed = TRUE)
-	text	<-	sub('. .','. ',text,fixed = TRUE)
+	text	<-	sub("\\n\\n","",text)
+	text	<-	sub("\n\n","",text)
+	text	<-	sub("\n","",text)
+	text	<-	sub("\\. . ","\\. ",text,fixed = TRUE)
+	text	<-	sub("\\.. ","\\. ",text,fixed = TRUE)
+	text	<-	sub(".. ",". ",text,fixed = TRUE)
 	return(text)
 }
