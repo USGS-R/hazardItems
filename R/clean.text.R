bbf4de614adaab5cc2b84dd416bccc9546ec2b6a
Jordan S Read <jread@usgs.gov>
2013-08-16 10:18:06 -0500

    added period arg

--- R/clean.text.R
+++ R/clean.text.R
@@ -1,4 +1,4 @@
-clean.text	=	function(text){
+clean.text	=	function(text,add.period=FALSE){
 	
 	text	<-	sub("\\n\\n","",text)
 	text	<-	sub("\n\n","",text)
@@ -6,5 +6,8 @@ clean.text	=	function(text){
 	text	<-	sub("\\. . ","\\. ",text,fixed = TRUE)
 	text	<-	sub("\\.. ","\\. ",text,fixed = TRUE)
 	text	<-	sub(".. ",". ",text,fixed = TRUE)
+	if (tail(text,1)!='.' && add.period){
+		text	<-	paste(text,'.',sep='')
+	}
 	return(text)
 }
