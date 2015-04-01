646a698949d587de3a6472386c6500bcf5baa5bb
Jordan S Read <jread@usgs.gov>
2015-04-01 15:11:41 -0500

    checks clean up

--- R/authenticateUser.R
+++ R/authenticateUser.R
@@ -2,8 +2,7 @@
 #'@description returns a token that can be used in POST or PUT methods
 #'@param username an active directory username (e.g., \code{'bbadger'})
 #'@param password a character password for the active directory username. 
-#'Will be prompted for password if missing and in interactive mode
-#'
+#'Will be prompted for password if missing and in interactive mode. 
 #'@details if \code{authenticateUser} is called with a \code{username} argument, 
 #'\code{username} is stored in the R session environment, and future calls to 
 #'\code{authenticateUser} within the same R session can use the password argument 
