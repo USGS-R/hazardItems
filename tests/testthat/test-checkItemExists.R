516f7435e571d895131f07cbe220ae369ea3a04d
Megan Hines <mhines@usgs.gov>
2015-04-22 14:20:24 -0700

    Removing unnecessary bits from testing of checkItemExists

--- tests/testthat/test-checkItemExists.R
+++ tests/testthat/test-checkItemExists.R
@@ -2,7 +2,7 @@ context("testing checkItemExists")
 
 test_that ("check if item exists", {
   setBaseURL("dev")
-  expect_false(hazardItems:::checkItemExists("CHEX123")) # bad itemID
-  expect_true(hazardItems:::checkItemExists("CAQw7M1")) # good itemID
+  expect_false(checkItemExists("CHEX123")) # bad itemID
+  expect_true(checkItemExists("CAQw7M1")) # good itemID
   
 })
