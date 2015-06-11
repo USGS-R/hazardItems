c3233a0396bf6eb82a51efa06c4c95c6ef29e050
Megan Hines <mhines@usgs.gov>
2015-06-11 09:25:13 -0500

    Stuff for saving track agg and kids

--- R/saveTrack.R
+++ R/saveTrack.R
@@ -20,24 +20,30 @@ saveTrack <- function(items, ...){
   checkAuth(...)
   
   makeCone <- function()
-    items <- getOption("hazardItems")$realtime.storms$track$
+    items <- getOption("hazardItems")$realtime.storms$track
+    serviceTypeSource <- "source_wms"
+    serviceTypeProxy <- "proxy_wms"
+    
     
   
   makeTrack <- function()
-    items <- getOption("hazardItems")$realtime.storms$track$
+    items <- getOption("hazardItems")$realtime.storms$track
+    serviceTypeSource <- "source_wms"
+    serviceTypeProxy <- "proxy_wms"
     
   makePoints <- function()
-    items <- getOption("hazardItems")$realtime.storms$track$
-  
+    items <- getOption("hazardItems")$realtime.storms$track
+    serviceTypeSource <- "source_wms"
+    serviceTypeProxy <- "proxy_wms"
+    
   makeParent <- function()
     items <- getOption("hazardItems")$realtime.storms$track
- 
-  ribbonable <- "false"
-  showChildren <- "false"
-  enabled <- "true"
-  activeStorm <- "false"
-  serviceTypeSource <- "source_wms"
-  serviceTypeProxy <- "proxy_wms"
+    ribbonable <- "false"
+    showChildren <- "false"
+    enabled <- "true"
+    activeStorm <- "false"
+    serviceTypeSource <- "source_wms"
+    serviceTypeProxy <- "proxy_wms"
   
   
 }
