e2235d175b0a65b405073bb59f7efbe211bcd56e
Jordan Walker <jiwalker@usgs.gov>
2015-06-11 11:51:41 -0500

    Initial modification of saveTrack, whitespace is a bit of an issue.

--- R/saveTrack.R
+++ R/saveTrack.R
@@ -3,47 +3,44 @@
 #'@param item list representing items to POST or PUT
 #'@param ... extra params to pass to checkAuth
 #'@return TRUE if it worked FALSE otherwise
-#'@importFrom httr POST PUT content http_status
-#'@importFrom jsonlite toJSON
 #'@examples
 #'\dontrun{
 #'setBaseURL('dev')
-#'  # create or replace NHC items
-#'       
-#'    
-#'\dontrun{
-#'setBaseURL('dev')
-#'saveItem(items, authenticateUser("username"))
+#'saveTrack()
 #'}
-#'@export 
-saveTrack <- function(items, ...){
+#'
+#'@export
+saveTrack <- function(...){
   checkAuth(...)
   
-  makeCone <- function()
-    items <- getOption("hazardItems")$realtime.storms$track
-    serviceTypeSource <- "source_wms"
-    serviceTypeProxy <- "proxy_wms"
-    
-    
+  trackConfig <- getOption("hazardItems")$realtime.storms$track
   
-  makeTrack <- function()
-    items <- getOption("hazardItems")$realtime.storms$track
-    serviceTypeSource <- "source_wms"
-    serviceTypeProxy <- "proxy_wms"
-    
-  makePoints <- function()
-    items <- getOption("hazardItems")$realtime.storms$track
-    serviceTypeSource <- "source_wms"
-    serviceTypeProxy <- "proxy_wms"
-    
-  makeParent <- function()
-    items <- getOption("hazardItems")$realtime.storms$track
-    ribbonable <- "false"
-    showChildren <- "false"
-    enabled <- "true"
-    activeStorm <- "false"
-    serviceTypeSource <- "source_wms"
-    serviceTypeProxy <- "proxy_wms"
-  
-  
+  children <- sapply(trackConfig$children, makeChild, trackConfig$serviceEndpoint)
+  item <- list(
+    id = trackConfig$id,
+    itemType = "aggregation",
+    name = "track",
+    type = "storms",
+    ribbonable = FALSE,
+    showChildren = FALSE,
+    bbox = bbox,
+    summary = summary,
+    children = children,
+    displayedChildren = children
+  )
+  trackId <- saveItem(item)
+  return(trackId == trackConfig$id)
+}
+
+makeChild = function(serviceParam, serviceEndpoint, bbox, summary) {
+  item <- list(itemType="data", name="track", type="storms", ribbonable=FALSE)
+  item$attr <- serviceParam
+  item$bbox <- bbox
+  item$summary <- summary
+  services = list()
+  append(services, list(type="source_wms", endpoint=serviceEndpoint, serviceParameter=serviceParam))
+  append(services, list(type="proxy_wms", endpoint=serviceEndpoint, serviceParameter=serviceParam))
+  item$services <- services
+  id <- saveItem(item)
+  return(id)
 }
