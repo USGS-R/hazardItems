033030cf50249b193d518639ac307445f1f02774
Megan Hines <mhines@usgs.gov>
2015-06-11 09:19:46 -0500

    Stuff for saving track agg and kids

---
+++ R/saveTrack.R
@@ -0,0 +1,43 @@
+#'@title save NHC items
+#'@description adds or updates the NHC items in the application
+#'@param item list representing items to POST or PUT
+#'@param ... extra params to pass to checkAuth
+#'@return TRUE if it worked FALSE otherwise
+#'@importFrom httr POST PUT content http_status
+#'@importFrom jsonlite toJSON
+#'@examples
+#'\dontrun{
+#'setBaseURL('dev')
+#'  # create or replace NHC items
+#'       
+#'    
+#'\dontrun{
+#'setBaseURL('dev')
+#'saveItem(items, authenticateUser("username"))
+#'}
+#'@export 
+saveTrack <- function(items, ...){
+  checkAuth(...)
+  
+  makeCone <- function()
+    items <- getOption("hazardItems")$realtime.storms$track$
+    
+  
+  makeTrack <- function()
+    items <- getOption("hazardItems")$realtime.storms$track$
+    
+  makePoints <- function()
+    items <- getOption("hazardItems")$realtime.storms$track$
+  
+  makeParent <- function()
+    items <- getOption("hazardItems")$realtime.storms$track
+ 
+  ribbonable <- "false"
+  showChildren <- "false"
+  enabled <- "true"
+  activeStorm <- "false"
+  serviceTypeSource <- "source_wms"
+  serviceTypeProxy <- "proxy_wms"
+  
+  
+}
